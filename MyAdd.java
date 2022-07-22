4d4802ace8b2b36e1d2291461e9b51bc49d619e3
MeshachOpoku <meshachboadi230@gmail.com>
2022-07-22 01:07:12 +0000

    PreCoding
    Momo,check even or odd and my is used to number

---
+++ MyAdd.java
@@ -0,0 +1,35 @@
+package Humanadvanced;
+import java.util.*;
+public class MyAdd {
+
+  public static void main(String[] args) {
+    double variable =0;
+//System.out.print("Adding how many numbers");
+Scanner add=new Scanner(System.in);
+Scanner is=new Scanner(System.in);
+Scanner 
+po=new Scanner(System.in);
+;int u=0;
+
+//System.out.print("Total is"+p);
+ //int gh=add.nextInt()
+ ;
+   double s[]=new double[100];
+   
+for(int h=1;u<10;u++) 
+{
+  System.out.println("Enter "+(h)+"  of numbers ");
+   s[u]=is.nextDouble();
+;
+ double ds=s[u];  h++; 
+  // MyAdd iq=new MyAdd();
+   ; variable+=ds;
+   
+};
+System.out.print("Sum is"+variable);
+ 
+ 
+}
+  
+
+}
